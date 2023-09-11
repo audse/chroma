1a3dcc418dd4d09235266b0dfd1bf891faad5a89
Audrey Serene <audrey.serene@gmail.com>
2023-09-11 15:57:34 -0500

    Initial Commit

---
+++ Chroma/ChromaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChromaApp.swift
+//  Chroma
+//
+//  Created by Audrey Serene on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ChromaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
